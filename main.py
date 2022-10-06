f98c129c3a9620e4782e0cfb1d7b0234c226674e
Aleksandr Nikolaev <usefulsan@yandex.ru>
2022-10-06 12:30:58 +0500

    small refactoring, commit to move to another pc

--- main.py
+++ main.py
@@ -8,3 +8,34 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+from utils import *
+
+
+def main():
+    while True:
+        user_input = input('Выберите действие:\n1. Сформировать файл с вакансиями с HH и Superjob\n2. Вывести список '
+                           'из вакансий c HH\n3. Вывести список вакансии с SJ\nВвод: ')
+        if int(user_input) == 1:
+            vacancy_1 = hh_data()
+            load_vacancy(vacancy_1)
+            vacancy_2 = sj_data()
+            load_vacancy(vacancy_2)
+            print('Файл загружен!')
+        if int(user_input) == 2:
+            vacancy_1 = hh_data()
+            for i in range(5):
+                print(i+1, vacancy_1[i])
+        if int(user_input) == 3:
+            vacancy_2 = sj_data()
+            for i in range(5):
+                print(i + 1, vacancy_2[i])
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
--- utils.py
+++ utils.py
@@ -11,22 +11,49 @@ def save_vacs(data: list) -> None:
 
 def get_hh_vac(req_text, how_many):
     hh = HH(req_text, how_many)
-    vacancies = []
-    for page in range(hh.iter):
-        data = hh.get_request(page)
-        json_res = json.loads(data.text)
-        for vac in json_res['items']:
-            vacancies.append(Vacancy(vac, hh=True))
-    save_vacs(vacancies)
+    vac = []
+    # for page in range(hh.iter):
+    #     data = hh.get_request(page)
+    #     json_res = json.loads(data.text)
+    #     for vac in json_res['items']:
+    #         vacancies.append(Vacancy(vac, hh=True))
+    # save_vacs(vacancies)
+    for i in range(0, how_many):
+
+        name = self.data['name']
+        reference = self.data['alternate_url']
+        if self.data['snippet']['requirement'] + self.data['snippet']['responsibility']:
+            description = self.data['snippet']['requirement'] + self.data['snippet']['responsibility']
+        else:
+            try:
+                description = self.data['snippen']['requirement']
+            except:
+                description = self.data['snippet']['responsibility']
+        try:
+            salary = self.data['salary']['from']
+        except:
+            salary = 0
+        vac.append(Vacancy(name, reference, description, salary))
+    save_vacs(vac)
 
 
 def get_sj_vac(name_job):
-    sj = Superjob(name_job)
-    sj.get_request(50)
+    vac = []
+    sj = Superjob('python')
+    response = sj.get_request()
+    soup = BeautifulSoup(response.text, 'lxml')
+    quotes = soup.find_all('span', class_="_9fIP1 _249GZ _1jb_5 QLdOc")
+    title = soup.find_all('span', class_="_9fIP1 _249GZ _1jb_5 QLdOc")
+    description = soup.find_all('span', class_="_1Nj4W _249GZ _1jb_5 _1dIgi _3qTky")
+    salary = soup.find_all('span', class_="_2eYAG _1nqY_ _249GZ _1jb_5 _1dIgi")
+    for i in range(0, len(quotes)):
+        name = title[i].find('a', href=True)
+        vac.append(Vacancy(quotes[i].text, name['href'], description[i].text, salary[i].text))
+    save_vacs(vac)
 
 
 print(get_sj_vac('python'))
-
+# print(get_hh_vac('python', 50))
 
 def load_vacancy(data: list):
     with open('vacancy.txt', 'a', encoding='utf-8') as file:
